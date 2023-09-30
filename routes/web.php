01e41811c43b7dbe6c2941351839514c0d204595
TrueWebMakers <75263159+truewebmakers@users.noreply.github.com>
2023-09-30 16:26:49 +0530

    App Up fina21

--- routes/web.php
+++ routes/web.php
@@ -38,8 +38,19 @@ Route::get('/run/queue', function () {
 Route::get('/run/command', function () {
     $type = request()->type;
     if($type == 'migrate'){
-        Artisan::call('migrate'); /
-    }    
+        Artisan::call('migrate'); 
+    }  
+    if($type == 'fresh'){
+        Artisan::call('migrate:fresh'); // Replace with the name of your custom command
+        Artisan::call('db:seed', ['--class' => 'AdminSeeder']);
+        return 'Scheduled task triggered successfully.';
+    } 
+    if($type == 'refresh'){
+        Artisan::call('migrate:refresh'); // Replace with the name of your custom command
+        Artisan::call('db:seed', ['--class' => 'AdminSeeder']);
+        return 'Scheduled task triggered successfully.';
+    } 
+   
     
     return 'Scheduled task triggered successfully.';
 });
