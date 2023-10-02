dccaaf4e13c9d53073dc74e243cfe45f2ab9d6a5
TrueWebMakers <75263159+truewebmakers@users.noreply.github.com>
2023-10-02 19:01:26 +0530

    change in migration3

--- app/Http/Controllers/VenueController.php
+++ app/Http/Controllers/VenueController.php
@@ -158,6 +158,7 @@ class VenueController extends Controller
 
     public function destroy($id)
     {
+        VenueSloting::where(['venue_address_id' => $id])->delete(); 
         $venue = VenueAddress::findOrFail($id);
         $venue->delete();
         return redirect()->route('venues.index')->with('success', 'Venue deleted successfully');
