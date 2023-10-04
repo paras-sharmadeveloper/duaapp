7abdf5889502fec8a96586e184787267f33b0bfd
TrueWebMakers <75263159+truewebmakers@users.noreply.github.com>
2023-10-04 22:22:35 +0530

    up2

--- app/Http/Controllers/VenueController.php
+++ app/Http/Controllers/VenueController.php
@@ -158,7 +158,7 @@ class VenueController extends Controller
 
     public function destroy($id)
     {
-        VenueSloting::where(['venue_address_id' => $id])->delete(); 
+        // VenueSloting::where(['venue_address_id' => $id])->delete(); 
         $venue = VenueAddress::findOrFail($id);
         $venue->delete();
         return redirect()->route('venues.index')->with('success', 'Venue deleted successfully');
