fb316e0df49a2d2747f8dd114e151a9e142b3392
TrueWebMakers <75263159+truewebmakers@users.noreply.github.com>
2023-10-04 21:03:07 +0530

    changes in form8

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -280,7 +280,7 @@
         <li class="nav-item dropdown pe-3">
 
           <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown" data-img="{{ Auth::user()->profile_pic }}">
-            @if (!empty(Auth::user()->profile_pic) && Storage::disk('s3_general')->exists('images/' . Auth::user()->profile_pic))
+            @if (!empty(Auth::check()) && Storage::disk('s3_general')->exists('images/' . Auth::user()->profile_pic))
             <img src="{{ env('AWS_GENERAL_PATH').'images/'.Auth::user()->profile_pic }}"  class="rounded-circle">
             @else
               <img src="{{ asset('assets/theme/img/profile-img.jpg') }}"  alt="Profile" class="rounded-circle">
