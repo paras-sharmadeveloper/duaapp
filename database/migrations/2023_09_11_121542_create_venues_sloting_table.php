94a727eac4a1bd1c2126f034101ae7a9d665290d
TrueWebMakers <75263159+truewebmakers@users.noreply.github.com>
2023-10-05 16:42:07 +0530

    changes in form48

--- database/migrations/2023_09_11_121542_create_venues_sloting_table.php
+++ database/migrations/2023_09_11_121542_create_venues_sloting_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->timestamps();
             $table->foreign('venue_address_id')
                   ->references('id')
-                  ->on('venue_addresses')->onDelete('cascade');
+                  ->on('venue_addresses')->onDelete('no action');
                   
         });
     }
