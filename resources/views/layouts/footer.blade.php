1b5ee41779ff8530df6e8470f861695c7f7d84ca
TrueWebMakers <75263159+truewebmakers@users.noreply.github.com>
2023-10-05 17:28:37 +0530

    changes in form55

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -47,7 +47,7 @@
     $(document).ready(function() {
         //Only needed for the filename of export files.
         //Normally set in the title tag of your page.
-        document.title = 'Simple DataTable';
+        // document.title = 'Simple DataTable';
         // DataTable initialisation
         $('.table').DataTable({
             "dom": '<"dt-buttons"Bf><"clear">lirtp',
