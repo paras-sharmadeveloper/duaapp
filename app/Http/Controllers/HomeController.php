f1b8de207e8c1fe711b2f6007ae18ed3d0dd68c1
TrueWebMakers <75263159+truewebmakers@users.noreply.github.com>
2023-10-06 18:00:01 +0530

    fresh24

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -429,7 +429,7 @@ class HomeController extends Controller
         $timezone = Timezone::where(['country_code' => $countryCode])->get()->first();
         Config::set('app.timezone', $timezone->timezone);
       }
-      $mytime = Carbon::now();
+      $mytime = Carbon::now()->tz($timezone->timezone);
       $currentTime = strtotime(now()->addHour(24)->format('Y-m-d H:i:s'));
       $EventStartTime = strtotime($venueAddress->venue_date .' '. $venueAddress->slot_starts_at);
       $slotsArr = [];
