188c760145b9f80fe70542ed8389c367c4520aff
TrueWebMakers <75263159+truewebmakers@users.noreply.github.com>
2023-11-03 15:42:10 +0530

    changes 57

--- resources/views/frontend/not-found.blade.php
+++ resources/views/frontend/not-found.blade.php
@@ -3,7 +3,7 @@
       <div class="row">
         <div class="col-sm-12 ">
           <div class="col-sm-10 col-sm-offset-1  text-center">
-            <div class="four_zero_four_bg">
+            <div class="four_zero_four_bg1">
               <h1 class="text-center ">404</h1>
   
             </div>
