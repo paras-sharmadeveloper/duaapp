7cdb6ac8444fcbf5fe7d56d93e0da92ac6aa3fdb
TrueWebMakers <75263159+truewebmakers@users.noreply.github.com>
2023-11-03 17:57:42 +0530

    changes 71

--- resources/views/site-admin/queue-list.blade.php
+++ resources/views/site-admin/queue-list.blade.php
@@ -30,6 +30,13 @@
             <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
         </div>
     @endif
+    @if(session()->has('error'))
+    <div class="alert alert-danger alert-dismissible fade show" role="alert">
+        <i class="bi bi-check-circle me-1"></i>
+        {{ session()->get('error') }}
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div> 
+    @endif 
     <div class="card">
         <div class="card-body">
             <h5 class="card-title">Manage Queue</h5>
