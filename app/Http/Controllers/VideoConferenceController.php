adc35b4fe4ae97170764349718aac97846074dba
TrueWebMakers <75263159+truewebmakers@users.noreply.github.com>
2023-11-03 22:29:45 +0530

    changes 126

--- app/Http/Controllers/VideoConferenceController.php
+++ app/Http/Controllers/VideoConferenceController.php
@@ -91,6 +91,7 @@ class VideoConferenceController extends Controller
             // $userTimeZone = "Asia/Kolkata";
             // $mytime = Carbon::now()->tz($userTimeZone);
             // $venueDateTme = '2023-11-04 10:00:00'; 
+            $venueDateTime = '2023-11-04 10:00:00'; 
             $meetingStartTime = Carbon::create($venueDateTme)->tz($userTimeZone);
             // $timeRemaining = $mytime->diffForHumans($meetingStartTime);
             $timeRemaining = $meetingStartTime->diffInHours($mytime);
