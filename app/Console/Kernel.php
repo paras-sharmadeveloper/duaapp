58ea0f3dbcc53ca442fd9a1674299dc748c5feec
TrueWebMakers <75263159+truewebmakers@users.noreply.github.com>
2023-11-15 20:25:03 +0530

    update OTP Tempalt22

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('queue:work --queue=default,high,low,create-slots,create-future-dates,send-message,send-email')
+        $schedule->command('queue:listen --queue=default,high,low,create-slots,create-future-dates,send-message,send-email')
         ->everyTwentySeconds()
         ->withoutOverlapping(); 
     }
