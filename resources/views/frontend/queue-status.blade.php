7dfc8734e87dd75a1b2291c226b8eb0eb3752e8d
TrueWebMakers <75263159+truewebmakers@users.noreply.github.com>
2023-12-20 11:19:48 +0530

    some changes15

--- resources/views/frontend/queue-status.blade.php
+++ resources/views/frontend/queue-status.blade.php
@@ -43,6 +43,7 @@
                         <p>{{ $venueAddress->slot_duration }} minutes 1 Question </p>
                         <div class="stats text-center">
                             <p>{{  $venueAddress->status_page_note }}</p>
+                            <p>{{ route('booking.status',[$userBooking->booking_uniqueid]) }}</p>
 
                             <button type="button" class="btn btn-success download-apponit" onclick="downloadPdf()">Download
                                 Appointment</button>
