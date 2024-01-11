e9c2ec3f150c3505d5fcbf02cc375fcd38f103a4
TrueWebMakers <75263159+truewebmakers@users.noreply.github.com>
2024-01-11 14:53:38 +0530

    change33

--- app/Http/Controllers/WhatsAppController.php
+++ app/Http/Controllers/WhatsAppController.php
@@ -148,7 +148,7 @@ class WhatsAppController extends Controller
             foreach ($venuesListArr as $venueDate) {
                 $columnToShow = $venueDate->combinationData->columns_to_show; 
                 $venueDate = $venueDate->venue_date; 
-                $slotMorng = $venueDate->slot_starts_at_morning; 
+                $slotMorng = $venueDate['slot_starts_at_morning']; 
                 $venueStartTime = strtotime(Carbon::parse( $venueDate.' ' .$slotMorng)); 
                 $todaydAteTime = strtotime(Carbon::now()->format('Y-m-d H:i:s')); 
 
