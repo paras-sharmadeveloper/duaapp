fed5e7c5653e01a920cf5f20ad6fc747ef976782
TrueWebMakers <75263159+truewebmakers@users.noreply.github.com>
2024-02-06 15:12:45 +0530

    changes10

--- database/migrations/2023_09_11_122452_create_vistors_table.php
+++ database/migrations/2023_09_11_122452_create_vistors_table.php
@@ -8,7 +8,7 @@ return new class extends Migration
 {
     /**
      * Run the migrations.
-     */ 
+     */
     public function up(): void
     {
         Schema::create('vistors', function (Blueprint $table) {
@@ -22,28 +22,28 @@ return new class extends Migration
             $table->string('phone',15)->nullable();
             // $table->string('phone',15)->unique();
             $table->enum('is_whatsapp',['yes','no'])->default('no');
-            $table->string('user_ip');
+            $table->string('user_ip')->nullable();
             $table->string('user_question')->nullable();
             $table->string('booking_uniqueid')->unique();
             $table->unsignedBigInteger('slot_id');
             $table->foreign('slot_id')
             ->references('id')
-            ->on('venues_sloting')->onDelete('cascade')->onUpdate('restrict');  
-            $table->string('meeting_type',60)->nullable(); 
-            $table->string('meeting_doneAt',60)->nullable(); 
-            $table->text('recognized_code')->nullable(); 
-            $table->timestamp('sms_sent_at')->nullable(); 
-            $table->timestamp('email_sent_at')->nullable(); 
-            $table->enum('is_available',['not_confirmed','confirmed'])->default('not_confirmed'); 
-            $table->string('confirmed_at',60)->nullable(); 
+            ->on('venues_sloting')->onDelete('cascade')->onUpdate('restrict');
+            $table->string('meeting_type',60)->nullable();
+            $table->string('meeting_doneAt',60)->nullable();
+            $table->text('recognized_code')->nullable();
+            $table->timestamp('sms_sent_at')->nullable();
+            $table->timestamp('email_sent_at')->nullable();
+            $table->enum('is_available',['not_confirmed','confirmed'])->default('not_confirmed');
+            $table->string('confirmed_at',60)->nullable();
             $table->enum('user_status',['in-queue','in-meeting','meeting-end','admitted','dismiss','hold','pause','blocked','no_action'])->default('no_action');
-            $table->string('meeting_start_at',60)->nullable(); 
-            $table->string('meeting_ends_at',60)->nullable(); 
-            $table->string('meeting_total_time',60)->nullable(); 
+            $table->string('meeting_start_at',60)->nullable();
+            $table->string('meeting_ends_at',60)->nullable();
+            $table->string('meeting_total_time',60)->nullable();
             $table->string('user_timezone',155)->nullable();
             $table->string('source',25)->nullable();
             $table->string('dua_type',25)->nullable();
-            $table->string('lang',25)->nullable();     
+            $table->string('lang',25)->nullable();
             $table->timestamps();
 
         });
