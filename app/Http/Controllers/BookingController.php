1fa16e56488ae70520622bd1dfd0249655a60ef2
TrueWebMakers <75263159+truewebmakers@users.noreply.github.com>
2024-02-07 18:36:21 +0530

    changes to website flo32

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -322,7 +322,7 @@ class BookingController extends Controller
             // echo url('assets/fonts/Jameel-Noori-Nastaleeq-Regular.ttf'); die;
             // echo public_path('assets/fonts/'); die;
             $mpdf = new Mpdf([
-                'fontDir' => public_path('assets/fonts/'), // Path to the directory containing Urdu font files
+                'fontDir' => '/assets/fonts/', // Path to the directory containing Urdu font files
                 'fontdata' => [
                     'urdu' => [
                         'R' => 'Jameel-Noori-Nastaleeq-Regular.ttf', // Replace with the actual font file name
