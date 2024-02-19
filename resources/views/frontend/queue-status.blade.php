b01e4b7ac3e5da29d0bac0ffec9754c569f2a9d1
TrueWebMakers <75263159+truewebmakers@users.noreply.github.com>
2024-02-19 16:19:04 +0530

    pdf changes2

--- resources/views/frontend/queue-status.blade.php
+++ resources/views/frontend/queue-status.blade.php
@@ -465,11 +465,11 @@
                     orientation: 'portrait'
                 }
             };
-            if (window.innerWidth < 768) {
+         //   if (window.innerWidth < 768) {
                 // Adjust options for mobile view
-                options.html2canvas.width = 1000; // Set the desired width for mobile view
-                options.html2canvas.height = 1200; // Set the desired height for mobile view
-            }
+            //    options.html2canvas.width = 1000; // Set the desired width for mobile view
+             //   options.html2canvas.height = 1200; // Set the desired height for mobile view
+           // }
             html2pdf(element, options);
             //  $(".download-apponit").hide();
         }
