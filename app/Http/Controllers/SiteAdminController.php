8661050a7a16d74370818f56e5861429a449d367
TrueWebMakers <75263159+truewebmakers@users.noreply.github.com>
2024-02-21 18:19:06 +0530

    status page adde24

--- app/Http/Controllers/SiteAdminController.php
+++ app/Http/Controllers/SiteAdminController.php
@@ -38,6 +38,10 @@ class SiteAdminController extends Controller
             ->where('venue_address_id', $id)
             ->has('visitors')
             ->get();
+            $venueSloting->each(function ($item) {
+                $item->visitors = $item->visitors->sortBy('confirmed_at');
+            });
+
             return response()->json(['success' => true , 'data' => $venueSloting],200);
         }
         $venueSloting = VenueSloting::with('visitors','venueAddress')
