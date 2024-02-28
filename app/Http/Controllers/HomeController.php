5a00bc7f8d25636906bfe377ac89e57d5f8baad8
TrueWebMakers <75263159+truewebmakers@users.noreply.github.com>
2024-02-28 18:34:56 +0530

    changes19

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -34,7 +34,7 @@ class HomeController extends Controller
 
     if($request->ajax()){
         $city = $request->input('city');
-        $venueAddress = VenueAddress::where(['city' => $city] )->where('venue_date', 'LIKE' ,date('Y-m-d').'%')->select('id','venue_date')->get();;
+        $venueAddress = VenueAddress::where(['city' => $city] )->where('venue_date','LIKE',"%{$today}%")->select('id','venue_date')->get();;
         return response()->json($venueAddress);
 
     }
