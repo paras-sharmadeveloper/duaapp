0bb235417dfa5af079123fa8d67953040087819e
TrueWebMakers <75263159+truewebmakers@users.noreply.github.com>
2024-04-22 16:52:03 +0530

    changes5

--- routes/web.php
+++ routes/web.php
@@ -215,6 +215,8 @@ Auth::routes(['register' => false]);
     Route::post('/book/sent-otp', [HomeController::class, 'SendOtpUser'])->name('send-otp');
     Route::post('/book/get-slots', [HomeController::class, 'getSlotsAjax'])->name('get-slots');
 
+    Route::get('/status', [HomeController::class, 'StatusLcdScreen'])->name('status-screen');
+
 Route::group(['middleware' => ['auth'], 'prefix' => 'admin'], function () {
     Route::get('/video/{bookingId}/join-conference', [VideoConferenceController::class, 'joinConferenceFrontend'])->name('join.conference.frontend');
     Route::get('/qr-code/{id}', [BookingController::class, 'generateQRCode'])->name('qr.code');
@@ -237,7 +239,7 @@ Route::group(['middleware' => ['auth'], 'prefix' => 'admin'], function () {
     Route::post('/book/check-available/slot', [HomeController::class, 'CheckAvilableSolt'])->name('check-available');
     Route::get('/booking/thankyou/{bookingId}', [HomeController::class, 'thankyouPage'])->name('thankyou-page');
     Route::get('/screen/status/{id}', [SiteAdminController::class, 'WaitingQueueShow'])->name('waiting-queue');
-    Route::get('/status', [HomeController::class, 'StatusLcdScreen'])->name('status-screen');
+
 
     Route::get('/get-states', [VenueCountryController::class, 'getStates'])->name('get-states');
     Route::post('/add-city-state', [VenueCountryController::class, 'CityImagesUplaod'])->name('add-city-state');
