897e2cb25b70ab8f76e17cf85badc82001c2966b
TrueWebMakers <75263159+truewebmakers@users.noreply.github.com>
2024-05-03 15:51:54 +0530

    working lady2

--- resources/views/layouts/guest-head.blade.php
+++ resources/views/layouts/guest-head.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
-    <title>{{ ucwords(request()->route()->getName()) }} - {{ env('APP_NAME') ?? 'Laravel' }}</title>
+    <title>kahayfaqeer.org</title>
     <meta content="" name="description">
     <meta content="" name="keywords">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
