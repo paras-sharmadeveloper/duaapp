97e73eb034c7e87e3e11bf06ba96a437130292bd
TrueWebMakers <75263159+truewebmakers@users.noreply.github.com>
2024-05-06 13:45:14 +0530

    mychange2

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -37,20 +37,22 @@ class HomeController extends Controller
             $dataMessage = $request->input('whatsAppMessage');
 
             foreach ($userMobile as $id => $phone) {
-                $visitors = Vistors::where('id',$id)->whereDate('created_at',date('Y-m-d'))->get();
 
+                $visitors = Vistors::where('id',$id)->whereDate('created_at',date('Y-m-d'))->get()->first();
                 foreach($visitors  as $visitor){
 
                     $dataMessage = str_replace('{token_url}',route('booking.status',[$visitor->booking_uniqueid]),$dataMessage);
                     $dataMessage = str_replace('{dua_type}', $visitor->dua_type ,$dataMessage);
                     $dataMessage = str_replace('{date}', date('d M Y', strtotime($visitor->created_at)) ,$dataMessage);
-                    $dataMessage = str_replace('{mobile}', $phone ,$dataMessage);
+                    $dataMessage = str_replace('{mobile}', $visitor->phone ,$dataMessage);
+
+                    $mobile =  $visitor->country_code .  $visitor->phone;
 
                     $message = <<<EOT
                         Please see below urgent message for your kind attention:
                         $dataMessage
                         EOT;
-                    $response =   $this->sendMessage($phone, $message);
+                    $response =   $this->sendMessage($mobile, $message);
                 }
 
                 // $message = <<<EOT
