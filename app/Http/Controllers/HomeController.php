dfe8da0c8d656ef069bc151cf5c19d237b15f4ff
TrueWebMakers <75263159+truewebmakers@users.noreply.github.com>
2024-05-24 21:19:34 +0530

    A7

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -496,8 +496,6 @@ class HomeController extends Controller
         $userArr = [];
 
         if (!empty($userAll)) {
-
-
             try {
                 $rekognition = new RekognitionClient([
                     'version' => 'latest',
@@ -507,7 +505,7 @@ class HomeController extends Controller
                         'secret' => env('AWS_SECRET_ACCESS_KEY'),
                     ],
                 ]);
-                Storage::disk('s3')->put($objectKey, $selfieImage);
+
 
                 foreach ($userAll as $user) {
 
@@ -536,6 +534,7 @@ class HomeController extends Controller
                         }
                     }
                 }
+                Storage::disk('s3')->put($objectKey, $selfieImage);
 
                 if (empty($userArr)) {
 
