c67ae9eefd36e80a59fcbb4a5487f2666b4361d7
TrueWebMakers <75263159+truewebmakers@users.noreply.github.com>
2024-05-24 15:24:18 +0530

    changes6

--- app/Http/Controllers/SiteAdminController.php
+++ app/Http/Controllers/SiteAdminController.php
@@ -55,6 +55,7 @@ class SiteAdminController extends Controller
                       ->orWhere('dua_type','working_lady_dua');
             })
             ->whereIn('user_status', ['in-meeting'])
+            ->whereDate('created_at',date('Y-m-d'))
             ->whereNotNull('confirmed_at')
             ->orderBy('slot_id', 'asc')
             ->first();
@@ -66,6 +67,7 @@ class SiteAdminController extends Controller
                       ->orWhere('dua_type','working_lady_dum');
             })
             ->whereIn('user_status', ['in-meeting'])
+            ->whereDate('created_at',date('Y-m-d'))
             ->whereNotNull('confirmed_at')
             ->orderBy('slot_id', 'asc')
             ->first();
