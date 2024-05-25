3f045208be52ff4857d4d4d4022dd7cb1260b02d
TrueWebMakers <75263159+truewebmakers@users.noreply.github.com>
2024-05-25 15:26:40 +0530

    clear objec26

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -559,13 +559,13 @@ class HomeController extends Controller
                                 'SourceImage' => [
                                     'S3Object' => [
                                         'Bucket' => $bucket,
-                                        'Name' => base64_encode(getImagefromS3($objectKey)),
+                                        'Name' => getObjectfromS3($objectKey),
                                     ],
                                 ],
                                 'TargetImage' => [
                                     'S3Object' => [
                                         'Bucket' => $bucket,
-                                        'Name' => base64_encode(getImagefromS3($user['recognized_code']))  ,
+                                        'Name' => getObjectfromS3($user['recognized_code']),
                                     ],
                                 ],
                             ]);
