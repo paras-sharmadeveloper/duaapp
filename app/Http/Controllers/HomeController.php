d607e13c3bba430716da0a3e50df2f3859b8e466
TrueWebMakers <75263159+truewebmakers@users.noreply.github.com>
2024-05-25 15:29:35 +0530

    clear objec27

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -559,13 +559,13 @@ class HomeController extends Controller
                                 'SourceImage' => [
                                     'S3Object' => [
                                         'Bucket' => $bucket,
-                                        'Name' => getObjectfromS3($objectKey),
+                                        'Name' => $objectKey,
                                     ],
                                 ],
                                 'TargetImage' => [
                                     'S3Object' => [
                                         'Bucket' => $bucket,
-                                        'Name' => getObjectfromS3($user['recognized_code']),
+                                        'Name' => $user['recognized_code'],
                                     ],
                                 ],
                             ]);
