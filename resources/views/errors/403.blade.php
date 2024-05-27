619c9b027099a05769836e0f21560bf6733f604e
TrueWebMakers <75263159+truewebmakers@users.noreply.github.com>
2024-05-27 20:31:53 +0530

    Update Working Lady Form Design6

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -8,7 +8,7 @@
     line-height: 150px;
 }
 @media (min-width: 992px) {
-    .error-404 img {
+    .error-404 .img-fluid {
         max-width: 20%;
     }
 }
@@ -17,8 +17,19 @@
     color: #fff;
     padding: 8px 30px;
 }
+body{
+    background-color:#000 ;
+}
+h2,h3{
+    color: #fff
+}
 </style>
 <section class="section error-404 min-vh-100 d-flex flex-column align-items-center justify-content-center">
+    <div class="d-flex justify-content-center py-4">
+        <a href="/" class="logoo  d-flex align-items-center wuto">
+            <img src="https://kahayfaqeer.org/assets/kahe-faqeer-white-1.png" alt="">
+        </a>
+    </div>
     <h1>403</h1>
     <h2>This website you can access only via mobile. Please open this website from your mobile phone.</h2>
 
