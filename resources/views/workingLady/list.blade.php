8ab9c9f3d08c5c92329aae5d8b295244adfa1a25
TrueWebMakers <75263159+truewebmakers@users.noreply.github.com>
2024-05-29 17:48:53 +0530

    Callback feature1

--- resources/views/workingLady/list.blade.php
+++ resources/views/workingLady/list.blade.php
@@ -15,7 +15,9 @@
         <table class="table-with-buttons table table-responsive cell-border">
             <thead>
                 <tr>
-                    <th>Info</th>
+                    <th>Full name </th>
+                    <th>Email</th>
+                    <th>Mobile </th>
                     <th>Designation</th>
                     <th>Employer Name</th>
                     <th>Place of Work</th>
@@ -26,29 +28,16 @@
             <tbody>
                 @foreach($registration as $list)
                 <tr>
-                    <td>{{ $list->first_name }} {{ $list->last_name }}
-                        <br>
-                        <span>{{ $list->email }}</span><br>
-                        <span>{{ $list->mobile }}</span><br>
-                        <span>{{ $list->email }}</span><br>
-                    </td>
-
+                    <td>{{ $list->first_name }} {{ $list->last_name }} </td>
+                    <td>{{ $list->email }}</td>
+                    <td>{{ $list->mobile }}</td>
                     <td>{{ $list->designation }}</td>
                     <td>{{ $list->employer_name }}</td>
                     <td>{{ $list->place_of_work }}</td>
                     <td>{{( $list->is_active =='active') ? 'Yes' : 'No' }}</td>
-
-                    <td>
-
-                        <a href="{{route('working.lady.view', $list->id) }}" class="btn btn-sm btn-primary">View</a>
+                    <td><a href="{{route('working.lady.view', $list->id) }}" class="btn btn-sm btn-primary">View</a></td>
 
 
-                        {{-- <form action="{{ route('reasons.destroy', $list->id) }}" method="POST" class="d-inline">
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit" class="btn btn-sm btn-danger" onclick="return confirm('Are you sure you want to delete this reason?')">Delete</button>
-                        </form> --}}
-                    </td>
                 </tr>
                 @endforeach
             </tbody>
