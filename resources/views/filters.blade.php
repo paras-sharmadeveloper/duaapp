db9c3dc677d6e0a5bde74f843f992131cc9fcc2a
TrueWebMakers <75263159+truewebmakers@users.noreply.github.com>
2024-05-29 17:19:08 +0530

    Callback feature5

--- resources/views/filters.blade.php
+++ resources/views/filters.blade.php
@@ -14,31 +14,31 @@
         $inactive = request()->get('inactive');
     @endphp
 
- @include('alerts')
-        <div class="card">
-            <div class="card-body">
-                <form action="{{ route('admin.filter')}}" method="get">
-                    <div class="row">
-                        <div class="col-md-4">
-                            <label> Filter Date </label>
-                            <input class="form-control" type="date" name="date">
-                        </div>
-                        <div class="col-md-4">
-
-                        </div>
-                        <div class="col-md-4">
-
-                        </div>
+    @include('alerts')
+    <div class="card">
+        <div class="card-body">
+            <form action="{{ route('admin.filter') }}" method="get">
+                <div class="row">
+                    <div class="col-md-4">
+                        <label> Filter Date </label>
+                        <input class="form-control" type="date" name="date">
                     </div>
-                    <button class="btn btn-secondary mt-4" type="submit">Filter </button>
-                </form>
-            </div>
+                    <div class="col-md-4">
+
+                    </div>
+                    <div class="col-md-4">
+
+                    </div>
+                </div>
+                <button class="btn btn-secondary mt-4" type="submit">Filter </button>
+            </form>
         </div>
+    </div>
 
     <div class="card">
         <div class="card-body">
             <h5 class="card-title">Search Entries</h5>
-            <table class="table-with-buttons table table-responsive cell-border">
+            <table class="table-with-buttons1 table table-responsive cell-border" id="tokenFilter">
                 <thead>
                     <tr>
                         <th>DbId</th>
@@ -47,83 +47,87 @@
                         <th>Source</th>
                         <th>Token Url</th>
                         <th>Token</th>
-                        <th>Token Session Image </th>
-                        <th>Working Lady Session Image </th>
-                        <th>Checkin Time Stamp (PK Time Zone)</th>
-                        <th>Token Print Count</th>
-                        <th>1st Msg Sent Status</th>
-                        <th>1st Msg Sent Date</th>
-                        <th>1st Msg Sid </th>
-                        <th>Status</th>
+                        <th style="width: 300px">Token Session Image </th>
+                        <th style="width: 300px">Working Lady Session Image </th>
+                        <th style="width: 300px">Checkin Time Stamp (PK Time Zone)</th>
+                        <th style="width: 300px">Token Print Count</th>
+                        <th style="width: 300px">1st Msg Sent Status</th>
+                        <th style="width: 300px">1st Msg Sent Date</th>
+                        <th style="width: 300px">1st Msg Sid </th>
+                        <th style="width: 300px">Status</th>
 
                         <th style="width: 300px">Action</th>
                     </tr>
                 </thead>
                 <tbody>
-                    @foreach ($visitors as  $visitor)
+                    @foreach ($visitors as $visitor)
+                        @php
+                            $image = !empty($visitor->recognized_code) ? getImagefromS3($visitor->recognized_code) : '';
+                            $workingLady = !empty($visitor->working_lady_id)
+                                ? getWorkingLady($visitor->working_lady_id)
+                                : [];
+                            $workingLadySession = !empty($workingLady)
+                                ? getImagefromS3($workingLady->session_image)
+                                : '';
 
-                    @php
-                          $image = (!empty($visitor->recognized_code))  ? getImagefromS3($visitor->recognized_code) : '';
-                          $workingLady = (!empty($visitor->working_lady_id)) ?  getWorkingLady($visitor->working_lady_id) : [];
-                          $workingLadySession = (!empty($workingLady)) ? getImagefromS3($workingLady->session_image) : '';
-
-                    @endphp
-                    <tr>
-                        <td>{{ $visitor->id }}</td>
-                        <td>{{ $visitor->dua_type }}</td>
-                        <td>{{ $visitor->phone }}</td>
-                        <td>{{ $visitor->source }}</td>
-                        <td><a href="{{ route('booking.status' , [$visitor->booking_uniqueid])}}" target="_blank"> Token Status </a> </td>
-                        <td>{{ $visitor->booking_number}}</td>
-                        <td>
-                            @if($image)
-                                <img src="data:image/jpeg;base64,{{ base64_encode($image) }}" alt="Preview Image"
-                                    style="height: 150px; width:150px;border-radius:20%">
+                        @endphp
+                        <tr>
+                            <td>{{ $visitor->id }}</td>
+                            <td>{{ $visitor->dua_type }}</td>
+                            <td>{{ $visitor->phone }}</td>
+                            <td>{{ $visitor->source }}</td>
+                            <td><a href="{{ route('booking.status', [$visitor->booking_uniqueid]) }}" target="_blank">
+                                    Token Status </a> </td>
+                            <td>{{ $visitor->booking_number }}</td>
+                            <td>
+                                @if ($image)
+                                    <img src="data:image/jpeg;base64,{{ base64_encode($image) }}" alt="Preview Image"
+                                        style="height: 150px; width:150px;border-radius:20%">
                                 @else
-                                <img src="https://kahayfaqeer-general-bucket.s3.amazonaws.com/na+(1).png" alt="Preview Image"
-                                style="height: 150px; width:150px;border-radius:20%">
-                            @endif
-                        </td>
-                        <td>
-                            @if($workingLadySession)
-                                <img src="data:image/jpeg;base64,{{ base64_encode($workingLadySession) }}" alt="Preview Image"
-                                    style="height: 150px; width:150px;border-radius:20%">
+                                    <img src="https://kahayfaqeer-general-bucket.s3.amazonaws.com/na+(1).png"
+                                        alt="Preview Image" style="height: 150px; width:150px;border-radius:20%">
+                                @endif
+                            </td>
+                            <td>
+                                @if ($workingLadySession)
+                                    <img src="data:image/jpeg;base64,{{ base64_encode($workingLadySession) }}"
+                                        alt="Preview Image" style="height: 150px; width:150px;border-radius:20%">
                                 @else
-                                <img src="https://kahayfaqeer-general-bucket.s3.amazonaws.com/na+(1).png" alt="Preview Image"
-                                style="height: 150px; width:150px;border-radius:20%">
-                            @endif
-                        </td>
-                        <td>
-                            {{ $visitor->confirmed_at }}
-                        </td>
-                        <td></td>
-                        <td>{{ $visitor->msg_sent_status }}</td>
-                        <td>{{ $visitor->msg_date }}</td>
-                        <td>{{ $visitor->msg_sid }}</td>
-                        <td>{{ $visitor->token_status }}</td>
+                                    <img src="https://kahayfaqeer-general-bucket.s3.amazonaws.com/na+(1).png"
+                                        alt="Preview Image" style="height: 150px; width:150px;border-radius:20%">
+                                @endif
+                            </td>
+                            <td>
+                                {{ $visitor->confirmed_at }}
+                            </td>
+                            <td></td>
+                            <td>{{ $visitor->msg_sent_status }}</td>
+                            <td>{{ $visitor->msg_date }}</td>
+                            <td>{{ $visitor->msg_sid }}</td>
+                            <td>{{ $visitor->token_status }}</td>
 
 
 
 
-                        <td>
-                            @if($visitor->token_status  == 'invaild')
-                                <form method="post" action="{{ route('admin.filter.status',[$visitor->id]) }}">
-                                    @csrf
-                                    <input type="hidden" name="status" value="vaild">
-                                    <button type="submit" class="btn btn-success">Vaild</button>
-                                </form>
-                            @endif
+                            <td>
+                                @if ($visitor->token_status == 'invaild')
+                                    <form method="post" action="{{ route('admin.filter.status', [$visitor->id]) }}">
+                                        @csrf
+                                        <input type="hidden" name="status" value="vaild">
+                                        <button type="submit" class="btn btn-success">Vaild</button>
+                                    </form>
+                                @endif
 
-                            @if($visitor->token_status  == 'vaild')
-                                <form method="post" action="{{ route('admin.filter.status',[$visitor->id]) }}">
-                                    @csrf
-                                    <input type="hidden" name="status" value="invaild">
-                                    <button type="submit" class="btn btn-danger">Invaild</button>
-                                </form>
-                            @endif
+                                @if ($visitor->token_status == 'vaild')
+                                    <form method="post" action="{{ route('admin.filter.status', [$visitor->id]) }}">
+                                        @csrf
+                                        <input type="hidden" name="status" value="invaild">
+                                        <button type="submit" class="btn btn-danger">Invaild</button>
+                                    </form>
+                                @endif
 
-                        </td>
-                    </tr>
+                            </td>
+                        </tr>
                     @endforeach
 
                 </tbody>
@@ -131,11 +135,28 @@
             </table>
         </div>
     </div>
-
 @endsection
 @section('page-script')
-<script>
+    <script>
+      $('#tokenFilter').DataTable({
+    "dom": 'lBfrtip',
+    "buttons": [
+        {
+            extend: 'pdfHtml5',
+            orientation: 'landscape'
+        },
+        'csv',
+        'excel'
+    ],
+    "columnDefs": [
+        {
+            "targets": [5], // index of the token_url_link column
+            "width": "500px" // set width to 500px
+        }
+    ],
+    "lengthMenu": [10, 25, 50, 100, 500, 1000, 1500, 2000, 2500, 3000],
+    "pageLength": 2000 // set default length to 1000
+});
 
-
-</script>
+    </script>
 @endsection
