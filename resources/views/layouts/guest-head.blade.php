b3cd4948ea1238d0848a1762a19be064d4768904
TrueWebMakers <75263159+truewebmakers@users.noreply.github.com>
2024-05-30 16:33:22 +0530

    design changes in print token8

--- resources/views/layouts/guest-head.blade.php
+++ resources/views/layouts/guest-head.blade.php
@@ -32,14 +32,18 @@
         }
     </style>
 
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-JKTQMG8C33"></script>
-    <script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
+    @if(request()->RouteIs('book.show'))
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-JKTQMG8C33"></script>
+        <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
 
-    gtag('config', 'G-JKTQMG8C33');
-    </script>
+        gtag('config', 'G-JKTQMG8C33');
+        </script>
+    @endif
+
+
 
 </head>
 
