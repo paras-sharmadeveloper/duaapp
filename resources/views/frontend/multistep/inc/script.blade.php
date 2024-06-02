97ab51e48f568b09c1d48d6be4740574e385528a
TrueWebMakers <75263159+truewebmakers@users.noreply.github.com>
2024-06-02 17:01:23 +0530

    as2

--- resources/views/frontend/multistep/inc/script.blade.php
+++ resources/views/frontend/multistep/inc/script.blade.php
@@ -637,11 +637,11 @@
                             $("#qr-code-listing").find(".card").addClass('active-card')
                             $("#mobile").val(response.data.mobile)
                             $("#working_lady_id").val(response.data.id)
-                            $("#cadr-1r").text('').hide()
+                            $(".alertBox").text('').hide()
 
                         } else {
 
-                            $("#cadr-1r").text(response.message).show()
+                            $(".alertBox").text(response.message).show()
 
 
                         // $("#qr-code-listing").find(".card").remove('active-card')
