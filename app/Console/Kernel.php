dc937be8b38ebd4f2b071f681821c31b7bb5c903
TrueWebMakers <75263159+truewebmakers@users.noreply.github.com>
2024-06-05 17:07:20 +0530

    fix user creat20

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         $schedule->command('queue:work --queue=default,high,low,create-slots,create-future-dates,whatsapp-notification-send')
-        ->everyTwentySeconds()
+        ->everyFiveMinutes()
         ->withoutOverlapping();
     }
 
