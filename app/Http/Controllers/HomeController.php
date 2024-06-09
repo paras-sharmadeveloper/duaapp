5c1222e6cbe688ef2a7d9e2df614627e48cbcc81
TrueWebMakers <75263159+truewebmakers@users.noreply.github.com>
2024-06-09 18:10:05 +0530

    changhes80

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -761,7 +761,7 @@ class HomeController extends Controller
             }
 
             session(['phoneCode' => $phoneCode]);
-        //    echo "<pre>"; print_r($userDetail); die(env('IP_API_KEY'));
+           echo "<pre>"; print_r($userDetail); die(env('IP_API_KEY'));
             $countryCode = $userDetail['countryCode'];
 
             $countryName = ucwords($userDetail['countryName']);
