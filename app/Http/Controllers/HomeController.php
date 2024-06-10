61778211fcdbe5a17ea49411551e4a5e86194514
TrueWebMakers <75263159+truewebmakers@users.noreply.github.com>
2024-06-10 08:33:47 +0530

    changhes107

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -262,15 +262,15 @@ class HomeController extends Controller
             }
 
             $captured_user_image = $request->input('captured_user_image');
-            if (!empty($captured_user_image)) {
-                $myImage = base64_decode(preg_replace('#^data:image/\w+;base64,#i', '', $captured_user_image));
-                $isUsers = $this->IsRegistredAlready($myImage, $rejoin);
-                if (!empty($isUsers) && $isUsers['status'] == false) {
-                    $end = microtime(true);
-                    $totalTime = $end - $start;
-                    return response()->json(['message' => $isUsers['message'], 'totalTime' => $totalTime, 'isUser' => $isUsers, "status" => false], 406);
-                }
-            }
+            // if (!empty($captured_user_image)) {
+            //     $myImage = base64_decode(preg_replace('#^data:image/\w+;base64,#i', '', $captured_user_image));
+            //     $isUsers = $this->IsRegistredAlready($myImage, $rejoin);
+            //     if (!empty($isUsers) && $isUsers['status'] == false) {
+            //         $end = microtime(true);
+            //         $totalTime = $end - $start;
+            //         return response()->json(['message' => $isUsers['message'], 'totalTime' => $totalTime, 'isUser' => $isUsers, "status" => false], 406);
+            //     }
+            // }
             $uuid = Str::uuid()->toString();
             $countryCode = $request->input('country_code');
             $country = Country::where(['phonecode' => $countryCode])->first();
