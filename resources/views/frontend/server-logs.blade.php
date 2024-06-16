8a1d6ae85b409e07efd92f7030d20f823dbb3a29
TrueWebMakers <75263159+truewebmakers@users.noreply.github.com>
2024-06-16 18:44:07 +0530

    changes22

--- resources/views/frontend/server-logs.blade.php
+++ resources/views/frontend/server-logs.blade.php
@@ -2,6 +2,7 @@
 @section('content')
 <div class="py-12">
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <h1>Server Logs</h1>
         <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
             <pre>{{ $logs }}</pre>
         </div>
