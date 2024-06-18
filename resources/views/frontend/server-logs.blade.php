a693615239cbfbd83ce3ff7a353e472beb2795bd
TrueWebMakers <75263159+truewebmakers@users.noreply.github.com>
2024-06-18 16:13:09 +0530

    Booking9

--- resources/views/frontend/server-logs.blade.php
+++ resources/views/frontend/server-logs.blade.php
@@ -3,7 +3,7 @@
 <div class="py-12">
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
         <h1>Server Logs</h1>
-        <a href="{{ route('admin.logs.clear')}}" class="btn btn-primary">Clear Log</a>
+        <a href="{{ route('admin.logs.clear')}}" class="btn btn-primary " style="margin: 0px 40px">Clear Log</a>
         <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
             <pre>{{ $logs }}</pre>
         </div>
