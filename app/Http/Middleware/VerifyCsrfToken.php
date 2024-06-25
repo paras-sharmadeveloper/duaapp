ba80aff112a986228ebcac6343db22ee9e4e82af
TrueWebMakers <75263159+truewebmakers@users.noreply.github.com>
2024-06-25 20:22:42 +0530

    mode to redis10

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,7 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        '/twilio/status/callback'
+        '/twilio/status/callback',
+        'horizon/*'
     ];
 }
