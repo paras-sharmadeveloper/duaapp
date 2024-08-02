d4258d858ec912c1f6da500a6741aa6be672b814
TrueWebMakers <75263159+truewebmakers@users.noreply.github.com>
2024-08-02 19:08:04 +0530

    changes15

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,14 +7,13 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
-use Spatie\Permission\Traits\HasRoles; 
+use Spatie\Permission\Traits\HasRoles;
 
 
 
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable,HasRoles;
-
     /**
      * The attributes that are mass assignable.
      *
@@ -24,7 +23,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
-        'profile_pic', 
+        'profile_pic',
         'status'
     ];
 
@@ -48,5 +47,5 @@ class User extends Authenticatable
         'password' => 'hashed',
     ];
 
-     
+
 }
