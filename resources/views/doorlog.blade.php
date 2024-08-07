764d013bf5f312569022f141602f391dab429a0c
TrueWebMakers <75263159+truewebmakers@users.noreply.github.com>
2024-08-07 22:01:50 +0530

    change10

--- resources/views/doorlog.blade.php
+++ resources/views/doorlog.blade.php
@@ -16,6 +16,7 @@
             <thead>
                 <tr>
                     <th>Sr. No</th>
+                    <th>TimeStamp</th>
                     <th> Dua Ghar </th>
                     <th> Date of Dua Day </th>
                     <th> Dua Type </th>
@@ -27,7 +28,7 @@
                     <th>DeviceID</th>
                     <th>ReaderNo</th>
                     <th>ActIndex</th>
-                    <th>CreatedAt</th>
+
 
                 </tr>
             </thead>
@@ -35,26 +36,18 @@
                 @foreach($logs as $list)
                 <tr>
                     <td>{{ $loop->iteration }}</td>
+                    <td>{{ $list->created_at }}</td>
                     <td> {{ ($list->visitor) ? $list->visitor->venueSloting->venueAddress->city : null }} </td>
                     <td> {{ ($list->visitor) ? $list->visitor->venueSloting->venueAddress->venue_date: null }} </td>
                     <td> {{ ($list->visitor) ? $list->visitor->venueSloting->type : null}} </td>
                     <td> {{ ($list->visitor) ? $list->visitor->phone : null }} </td>
                     <td> {{ ($list->visitor) ? $list->visitor->booking_number : null}} </td>
-                    <td>
-                        <a href="{{ route('booking.status',$list->visitor->booking_uniqueid)  }} " target="_blank"> Token Url</a>
-
-                        </td>
-
-
+                    <td> <a href="{{ route('booking.status',$list->visitor->booking_uniqueid)  }} " target="_blank"> Token Url</a> </td>
                     <td>{{ $list->SN }}</td>
                     <td>{{ $list->SCode }}</td>
                     <td>{{ $list->DeviceID }}</td>
                     <td>{{ $list->ReaderNo }}</td>
                     <td>{{ $list->ActIndex }}</td>
-                    <td>{{ $list->created_at }}</td>
-
-
-
 
                 </tr>
                 @endforeach
