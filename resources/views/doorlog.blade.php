42f6ed473539c30e2c8c2c26c951f374b2466e61
TrueWebMakers <75263159+truewebmakers@users.noreply.github.com>
2024-08-07 21:54:22 +0530

    change4

--- resources/views/doorlog.blade.php
+++ resources/views/doorlog.blade.php
@@ -16,26 +16,36 @@
             <thead>
                 <tr>
                     <th>Sr. No</th>
+                    <th> Dua Ghar </th>
+                    <th> phone </th>
+                    <th> Token Number </th>
+                    <th> Token URL </th>
                     <th>Sn</th>
                     <th>SCode </th>
                     <th>DeviceID</th>
                     <th>ReaderNo</th>
                     <th>ActIndex</th>
                     <th>CreatedAt</th>
-                    <th> phone </th>
+
                 </tr>
             </thead>
             <tbody>
                 @foreach($logs as $list)
                 <tr>
                     <td>{{ $loop->iteration }}</td>
+                    <td> {{ $list->visitor->venueSloting->venueAddress->address }} </td>
+                    <td> {{ $list->visitor->phone }} </td>
+                    <td> {{ $list->visitor->booking_number }} </td>
+                    <td> {{ route('booking.status',$list->visitor->booking_uniqueid)  }} </td>
+
+
                     <td>{{ $list->SN }}</td>
                     <td>{{ $list->SCode }}</td>
                     <td>{{ $list->DeviceID }}</td>
                     <td>{{ $list->ReaderNo }}</td>
                     <td>{{ $list->ActIndex }}</td>
                     <td>{{ $list->created_at }}</td>
-                    <td> {{ $list->visitor->phone }}
+
 
 
 
