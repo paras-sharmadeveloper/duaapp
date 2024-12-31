ffeddab6b7bf19786b4b6f734427d5c032065114
TrueWebMakers <75263159+truewebmakers@users.noreply.github.com>
2024-12-31 16:40:45 +0530

    chnages2

--- app/Http/Controllers/ManualBookingController.php
+++ app/Http/Controllers/ManualBookingController.php
@@ -37,9 +37,10 @@ class ManualBookingController extends Controller
         $targetDate = Carbon::parse($date );
 
         $phoneNumbers = VisitorTempEntry::whereDate('created_at', $targetDate)->distinct('phone')->get(['phone','created_at','venueId']);
+        $phoneNumbers = VisitorTempEntry::with('venueAddress')->first();
 
 
-        echo "<pre>"; print_r($phoneNumbers); die;
+        // echo "<pre>"; print_r($phoneNumbers); die;
         $venueId = $phoneNumbers->isNotEmpty() ? $phoneNumbers[0]->venueId : null;
             if ($venueId) {
                 $venueAddress = VenueAddress::find($venueId, ['repeat_visitor_days', 'id']);
