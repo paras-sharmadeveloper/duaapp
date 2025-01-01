290f1d20fd9a389a3ece1808787ce13e1248a246
TrueWebMakers <75263159+truewebmakers@users.noreply.github.com>
2025-01-01 19:11:43 +0530

    chnages62

--- resources/views/manualBooking/list.blade.php
+++ resources/views/manualBooking/list.blade.php
@@ -115,7 +115,7 @@
                     </form>
                 </div>
             </div>
-            <table id="visitorTable" class="table-with-button1s table table-responsive cell-border">
+            <table id="visitorTable" class=" ">
                 <thead>
                     <tr>
                         <th>
