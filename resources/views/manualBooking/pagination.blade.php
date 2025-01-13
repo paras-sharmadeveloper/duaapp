6916446af60fee988c2ff56101b42c836a0a8ecf
TrueWebMakers <75263159+truewebmakers@users.noreply.github.com>
2025-01-13 12:59:10 +0530

    ca12

--- resources/views/manualBooking/pagination.blade.php
+++ resources/views/manualBooking/pagination.blade.php
@@ -166,7 +166,7 @@
                             <div class="col-md-4">
                                 <label> Search Action Type</label>
                                 <select class="form-control" name="action_type">
-                                    <option  @if (request()->get('action_type') != 'dua' || request()->get('action_type') != 'dum' ) ? selected : '' @endif > Choose  Status  </option>
+                                    <option  @if (request()->get('action_type') != 'dua' || request()->get('action_type') != 'dum' ) ? '' : selected @endif > Choose  Status  </option>
                                     <option @if (request()->get('action_type') == 'dua') ? selected : '' @endif value="approved"> Approved
                                     </option>
                                     <option @if (request()->get('action_type') == 'dum') ? selected : '' @endif value="disapproved"> Disapproved
